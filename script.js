1f6e094c4e07dd26b14c3ca2eb1073e613eabc31
melmoara-maker <mel_moara@hotmail.com>
2026-07-15 10:47:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const abrir = document.getElementById("abrir");
+const convite = document.getElementById("convite");
+const musica = document.getElementById("musica");
 
+abrir.addEventListener("click", () => {
+    convite.style.display = "block";
+
+    musica.play().catch(() => {
+        console.log("A reprodução automática foi bloqueada pelo navegador.");
+    });
+
+    convite.scrollIntoView({
+        behavior: "smooth"
+    });
+});
